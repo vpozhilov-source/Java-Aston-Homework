4df2d4abf9bcb42a39846e126efaf8527c1c49e9
vpozhilov-source <v.pozhilov@emergingtravel.com>
2025-09-23 11:57:38 +0400

    Add method 1 - printThreeWords

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -1,5 +1,15 @@
 public class HomeWorkApp {
+    
+    // Метод 1: Печатает три слова в столбец
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        // Проверяем метод 1
+        System.out.println("=== Метод 1 ===");
+        printThreeWords();
     }
 }
