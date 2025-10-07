f1561893de1bf885d040c2e07fbabc2b49d4ddc3
vpozhilov-source <v.pozhilov@emergingtravel.com>
2025-10-07 18:57:41 +0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,61 +1,50 @@
 public class Main {
-    static class MyArraySizeException extends Exception {
-        public MyArraySizeException(String message) {
-            super(message);
-        }
-    }
-    static class MyArrayDataException extends Exception {
-        public MyArrayDataException(String message) {
-            super(message);
-        }
-    }
-    
-    public static void checkSize(String[][] array) throws MyArraySizeException {
-        if (array.length != 4) {
-            throw new MyArraySizeException("Array must be 4x4! Received rows: " + array.length);
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].length != 4) {
-                throw new MyArraySizeException("Row " + i + " must have 4 elements! Received: " + array[i].length);
-            }
-        }
-    }
-    
-    public static int sumArrayElements(String[][] array) throws MyArrayDataException {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                try {
-                    int number = Integer.parseInt(array[i][j]);
-                    sum = sum + number;
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Error in the cell [" + i + "][" + j + "]");
-                }
-            }
-        }
-        return sum;
-    }
-    
-    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
-        checkSize(array);              
-        return sumArrayElements(array); 
-    }
-    
     public static void main(String[] args) {
-        String[][] array4x4 = {
+        String[][] correctArray = {
             {"1", "2", "3", "4"},     
             {"5", "6", "7", "8"},     
             {"9", "10", "11", "12"},  
             {"13", "14", "15", "16"}
         };
         
+        String[][] wrongDataArray = {
+            {"1", "2", "3", "4"},     
+            {"5", "6", "seven", "8"},
+            {"9", "10", "11", "12"},  
+            {"13", "14", "15", "16"}
+        };
+        
+        String[][] wrongSizeArray = {
+            {"1", "2", "3"},     
+            {"5", "6", "7"}
+        };
+        
         try {
-            int result = processArray(array4x4);
-            System.out.println("Size is right! Sum: " + result);
-        } catch (MyArraySizeException e) {
-            System.out.println("Size error: " + e.getMessage());
-        } catch (MyArrayDataException e) {
-            System.out.println("Data error: " + e.getMessage()); 
+            int result = ArrayProcessor.processArray(correctArray);
+            System.out.println("Sum: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        try {
+            int result = ArrayProcessor.processArray(wrongDataArray);
+            System.out.println("Sum: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        try {
+            int result = ArrayProcessor.processArray(wrongSizeArray);
+            System.out.println("Sum: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        try {
+            int[] arr = {1, 2, 3};
+            System.out.println(arr[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException: " + e.getMessage());
         }
     }
 }
