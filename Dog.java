0f702cf3d7e8f36facf3ecb096dcb40fdab2de89
vpozhilov-source <v.pozhilov@emergingtravel.com>
2025-10-07 18:41:07 +0400

    Dog.java

---
+++ Dog.java
@@ -0,0 +1,32 @@
+public class Dog extends Animal {
+    private static int dogCount = 0;
+    private static final int MAX_RUN_DISTANCE = 500;
+    private static final int MAX_SWIM_DISTANCE = 10;
+    
+    public Dog(String name) {
+        super(name);
+        dogCount++;
+    }
+    
+    @Override
+    public void run(int distance) {
+        if (distance <= MAX_RUN_DISTANCE) {
+            System.out.println(name + " пробежал " + distance + " м.");
+        } else {
+            System.out.println(name + " не может пробежать " + distance + " м.");
+        }
+    }
+    
+    @Override
+    public void swim(int distance) {
+        if (distance <= MAX_SWIM_DISTANCE) {
+            System.out.println(name + " проплыл " + distance + " м.");
+        } else {
+            System.out.println(name + " не может проплыть " + distance + " м.");
+        }
+    }
+    
+    public static int getDogCount() {
+        return dogCount;
+    }
+}
