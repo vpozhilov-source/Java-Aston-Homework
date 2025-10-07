b063386f8a085e97919c9bbb84ef2159040b929e
vpozhilov-source <v.pozhilov@emergingtravel.com>
2025-10-07 18:39:32 +0400

    Animal.java

--- Animal.java
+++ Animal.java
@@ -1,1 +1,16 @@
-
+public abstract class Animal {
+    protected String name;
+    protected static int animalCount = 0;
+    
+    public Animal(String name) {
+        this.name = name;
+        animalCount++;
+    }
+    
+    public abstract void run(int distance);
+    public abstract void swim(int distance);
+    
+    public static int getAnimalCount() {
+        return animalCount;
+    }
+}
